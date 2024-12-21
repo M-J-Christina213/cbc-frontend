97c283a3e74f6a23387bad4b5ca5f4f5cc3fd014
Merrian Jethuni <m.j.christina321@gmail.com>
2024-12-21 11:55:38 +0530

    Added UserData component to display user profile details and status 🆕

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ProductCart from './components/productCard'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <ProductCart> 
+
+    </ProductCart>
+    <ProductCart> 
+      
+    </ProductCart>
       
     </>
   )
---
+++ src/components/userData.jsx
@@ -0,0 +1,11 @@
+export default function UserData(){
+    return(
+    <>
+       <img src="C:\Users\Jethuni\OneDrive\Desktop\Courses\websites - wordpress\Assignments\Assignment 1\Images for properties\Agents\Agent Sophia Bennett.jpeg"/>
+       <span> Malith Dilshan </span>
+       <span> Online </span>
+    </>
+   )
+
+}
+    
