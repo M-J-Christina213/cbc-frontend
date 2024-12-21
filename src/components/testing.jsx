fb1770ca3bd5865cb2fef1c73b2f1fa03e3cc64c
Merrian Jethuni <m.j.christina321@gmail.com>
2024-12-21 14:43:44 +0530

    ⚡ Added increment function and button functionality to Testing component

--- src/components/testing.jsx
+++ src/components/testing.jsx
@@ -1,10 +1,15 @@
 import "./testing.css"
 export default function Testing(){
+    let count = 0;
+
+    function increment(){
+        count = count + 1
+    }
     return (
         <div className="background">
             <button>-</button>
-            <span>0</span>
-            <button>+</button>
+            <span>{count}0</span>
+            <button onClick={}>+</button>
         </div>
     )
 }
