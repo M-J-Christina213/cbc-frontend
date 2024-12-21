723f6cd0082f7e29ff7c952a41b15b471ad6caf9
Merrian Jethuni <m.j.christina321@gmail.com>
2024-12-21 11:58:28 +0530

    Added UserData component alongside ProductCart 🔗

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import ProductCart from './components/productCard'
+import UserData from './components/userData'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,8 +14,11 @@ function App() {
 
     </ProductCart>
     <ProductCart> 
-      
+
     </ProductCart>
+    <UserData> 
+      
+    </UserData>
       
     </>
   )
