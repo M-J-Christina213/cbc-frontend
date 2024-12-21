8fa753adb7376569c6874c9fdeaaf8549bca778c
Merrian Jethuni <m.j.christina321@gmail.com>
2024-12-21 11:34:53 +0530

    ✨ Created ProductCart function in components folder to display product cards

---
+++ src/components/productCard.jsx
@@ -0,0 +1,15 @@
+export default function ProductCart(){
+    return (
+        <div>
+            <h1>
+                Product One
+            </h1>
+            <h2>
+                Price: $100
+            </h2>
+            <button>
+                Add to Cart
+            </button>
+        </div>
+    )
+}
