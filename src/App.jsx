5ca44cf6cbeca23e4b9d29b9a07f56b4631d845c
Merrian Jethuni <m.j.christina321@gmail.com>
2024-12-23 20:13:42 +0530

    🚀 Set up react-router-dom with BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,16 @@ import UserData from './components/userData'
 import Testing from './components/testing'
 import LoginPage  from './pages/loginPage'
 import Homepage from './pages/homePage'
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <Homepage/>
+    <BrowserRouter>
+    </BrowserRouter>
+     
     </>
   )
 }
