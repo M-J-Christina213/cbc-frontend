e6ab0c238df876f93b2aae23592cf6ceec55c4ad
Merrian Jethuni <m.j.christina321@gmail.com>
2024-12-23 18:57:44 +0530

    Imported LoginPage component fixed🔧

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import ProductCart from './components/productCard'
 import UserData from './components/userData'
 import Testing from './components/testing'
+import LoginPage from './pages/loginPage'
 
 function App() {
   const [count, setCount] = useState(0)
