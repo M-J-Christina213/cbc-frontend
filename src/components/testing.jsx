efe91837b9107f7f9376a00bba3f4b3bf7739d68
Merrian Jethuni <m.j.christina321@gmail.com>
2024-12-23 13:58:00 +0530

    Added name state with default value "Students" in Testing component 🎉

--- src/components/testing.jsx
+++ src/components/testing.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 import "./testing.css"
 export default function Testing(){
     const [count,setCount] = useState(0)
-    const [name, setName] = useState("")
+    const [name, setName] = useState("Students")
 
     function increment(){
         setCount(count + 1)
@@ -14,6 +14,7 @@ export default function Testing(){
     }
     return (
         <div className="background">
+            <h1>{name}</h1>
             <button onClick={decrement}>-</button>
             <span>{count}</span>
             <button onClick={increment}>+</button>
