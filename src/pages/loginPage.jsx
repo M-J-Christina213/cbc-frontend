477695ed5a523a94f777e1d711128de7435dba50
Merrian Jethuni <m.j.christina321@gmail.com>
2024-12-24 12:03:00 +0530

    🌀Replaced loginpage.css and homepage.css with Tailwind css ✨"

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -6,7 +6,7 @@ export default function LoginPage(){
             <h1> Login Page </h1>
             <input type="text" placeholder="Enter your username"/>
             <input type="password" placeholder="Enter your password"/>
-            <button className='btn-blue round-corners font-white'> Login </button>
+            <button className=''> Login </button>
             <Link to="/"> Home </Link>
         </div>
     )
