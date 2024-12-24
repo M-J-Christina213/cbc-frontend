a9307717650fdb6d6d7b8bd492e24e45829fdd49
Merrian Jethuni <m.j.christina321@gmail.com>
2024-12-24 12:10:54 +0530

    Removed elements from LoginPage for padding adjustments 🔧

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -3,11 +3,7 @@ import { Link } from 'react-router-dom';
 export default function LoginPage(){
     return (
         <div className="container">
-            <h1> Login Page </h1>
-            <input type="text" placeholder="Enter your username"/>
-            <input type="password" placeholder="Enter your password"/>
-            <button className=''> Login </button>
-            <Link to="/"> Home </Link>
+            <button className='bg-yellow-500'> Login </button>
         </div>
     )
 }
