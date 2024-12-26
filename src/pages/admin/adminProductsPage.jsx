c08d44f32a70324a226fe01057fbb8d82ceeb045
Merrian Jethuni <m.j.christina321@gmail.com>
2024-12-26 23:40:45 +0530

    😄 Added async function to fetch products in AdminProductsPage component 🛍️

--- src/pages/admin/adminProductsPage.jsx
+++ src/pages/admin/adminProductsPage.jsx
@@ -4,10 +4,7 @@ import { Link } from 'react-router-dom';
 
 export default function AdminProductsPage(){
 
-    axios.get("http://localhost:5000/api/products").then((res)=>{
-        console.log(res);
-    })
-    
+   getProducts()
     return(
         <div>
             <h1> Admin Products Page </h1> 
@@ -54,9 +51,14 @@ export default function AdminProductsPage(){
           </Link>
         </div>
       </div>
+    )
 
+    async function getProducts(){
+        const res = await axios.get("http://localhost:5000/api/products")
+
+        console.log(res)
+    }
     
     
       
-    )
 }
