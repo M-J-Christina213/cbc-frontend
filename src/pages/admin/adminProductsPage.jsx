77115aa032cd569bfff1d515c8609126a111b956
Merrian Jethuni <m.j.christina321@gmail.com>
2024-12-26 22:31:09 +0530

    Added API call to fetch products in the AdminProductsPage using Axios  🚀

--- src/pages/admin/adminProductsPage.jsx
+++ src/pages/admin/adminProductsPage.jsx
@@ -1,4 +1,10 @@
+import axios from 'axios';
+
 export default function AdminProductsPage(){
+
+    axios.get("http://localhost:5000/api/products").then((res)=>{
+        console.log(res);
+    })
     
     return(
         <div>
