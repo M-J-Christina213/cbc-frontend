58400a07420fb96c87de78a832da50773b0a0f17
Merrian Jethuni <m.j.christina321@gmail.com>
2025-01-04 13:07:38 +0530

    Integrated Supabase for image uploads and added file validation for jpg/png  🚀

--- src/pages/home/test.jsx
+++ src/pages/home/test.jsx
@@ -1,4 +1,9 @@
+import { createClient } from "@supabase/supabase-js"
 import { useState } from "react"
+
+const key = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpyemtvdW5iZGxmaHZoYndvempyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU4MzA0NTcsImV4cCI6MjA1MTQwNjQ1N30.EsHykQ18bY6mOHFYBiiUIANEdN2lQItP0d36wSgOGWY`
+
+const url = "https://zrzkounbdlfhvhbwozjr.supabase.co"
 
 export default function FileUploadTest(){
 
@@ -9,6 +14,24 @@ export default function FileUploadTest(){
             return
         }
         console.log(file)
+
+        const fileName = file.name
+
+        const extension = fileName.spilt(".")[fileName.split(".").length-1] 
+
+        if(extension != "jpg" && extension != "png"){
+            alert ("Please select a jpg or png file")
+            return
+        }
+
+        const supabase = createClient(url, key)
+
+        supabase.storage.from("images").upload(file.name, file, {
+            cacheControl : "3600",
+            upsert : false
+        }).then((res)=>{
+            console.log(res)
+        })
     }
     return (
         <div>
