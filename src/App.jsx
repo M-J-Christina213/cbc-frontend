a1d041233c829b04c82e21e83028c46e9310e656
Merrian Jethuni <m.j.christina321@gmail.com>
2025-01-04 12:06:57 +0530

    ✨ Added FileUploadTest component and /test route for file upload functionality 🤨

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import './App.css'
 import ProductCart from './components/productCard'
 import UserData from './components/userData'
-import Testing from './components/testing'
 import LoginPage  from './pages/loginPage'
 import Homepage from './pages/homePage'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import SignUpPage from './pages/signUpPage'
 import AdminHomepage from './pages/admin/adminHomePage'
 import { Toaster } from 'react-hot-toast'
+import FileUploadTest from './pages/home/test'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -23,6 +23,7 @@ function App() {
       <Route path='/signup' element={<SignUpPage/>}/>
       <Route path="/admin/*" element={<AdminHomepage/>}/>
       <Route path="/*" element={<Homepage/>} />
+      <Route path="/test" element={<FileUploadTest/>} />
     </Routes>
     </BrowserRouter>
      
