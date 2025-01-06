4e8b2ff61fd04eb7ea545efa644da0ee191c9364
Merrian Jethuni <m.j.christina321@gmail.com>
2025-01-06 22:26:49 +0530

    Added Supabase integration for media upload functionality ✨

--- src/pages/home/test.jsx
+++ src/pages/home/test.jsx
@@ -1,56 +1,11 @@
 import { createClient } from "@supabase/supabase-js"
 import { useState } from "react"
 
-const key = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpyemtvdW5iZGxmaHZoYndvempyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU4MzA0NTcsImV4cCI6MjA1MTQwNjQ1N30.EsHykQ18bY6mOHFYBiiUIANEdN2lQItP0d36wSgOGWY`
-
-const url = "https://zrzkounbdlfhvhbwozjr.supabase.co"
-
 export default function FileUploadTest(){
 
     const [file, setFile] = useState(null)
     function handleUpload(){
-        if (file==null){
-            alert("Please select a file")
-            return
-        }
-        console.log(file)
-
-        const fileName = file.name
-
-        const extension = fileName.split(".")[fileName.split(".").length-1] 
-
-        if(extension != "jpg" && extension != "png"){
-            alert ("Please select a jpg or png file")
-            return
-        }
-
-        const supabase = createClient(url, key)
-
-        const timestamp = new Date().getTime()
-
-        fileName = timestamp+"."+extension
-
-        supabase.storage.from("images").upload(file.name, file, {
-            cacheControl : "3600",
-            upsert : false
-        }).then(({ data, error }) => {
-            if (error) {
-              console.error("Upload Error:", error);
-              return;
-            }
-            console.log("Upload Success:", data);
-        
-            // Get public URL
-      const { data: publicUrlData, error: urlError } = supabase.storage
-      .from("images")
-      .getPublicUrl(file.name);
-
-    if (urlError) {
-      console.error("Public URL Error:", urlError);
-    } else {
-      console.log("Public URL:", publicUrlData.publicUrl);
-    }
-          });
+       
 
         }
     return (
---
+++ src/utilis/mediaUpload.jsx
@@ -0,0 +1,41 @@
+import { createClient } from "@supabase/supabase-js"
+
+const key = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpyemtvdW5iZGxmaHZoYndvempyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU4MzA0NTcsImV4cCI6MjA1MTQwNjQ1N30.EsHykQ18bY6mOHFYBiiUIANEdN2lQItP0d36wSgOGWY`
+
+const url = "https://zrzkounbdlfhvhbwozjr.supabase.co"
+
+export default function uploadMediaToSupabase(file){
+    return new Promise((resolve, reject)=>{
+        if (file==null){
+            reject("file not added")
+        }
+
+        let fileName = file.name
+
+        const extension = fileName.split(".")[fileName.split(".").length-1] 
+
+        const supabase = createClient(url, key)
+
+        const timestamp = new Date().getTime()
+    
+        fileName = timestamp+"."+extension
+
+        supabase.storage.from("images").upload(file.name, file, {
+            cacheControl : "3600",
+            upsert : false
+        }).then(()=>{
+          const publicUrl = supabase.storage.from("images").getPublicUrl(fileName).data.publicUrl;
+          resolve(publicUrl);
+        }).catch((err)=>{
+            reject(err)
+        })
+
+    })
+
+    if(extension != "jpg" && extension != "png"){
+        alert ("Please select a jpg or png file")
+        return
+    }
+
+   
+}
