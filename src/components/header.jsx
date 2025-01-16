bc4667466df1fe430f912e738ffbb884171eca08
Merrian Jethuni <m.j.christina321@gmail.com>
2025-01-17 00:19:11 +0530

    ✨  Center logo and add Home link

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,11 @@
-export default function Header(){
-    return (
-        <header className="bg-white w-full h-[50px]" >
+import {Link } from "react-router-dom"
 
-        </header>
-    )
-}
+export default function Header() {
+    return (
+      <header className="bg-white w-full h-[100px] relative flex justify-center items-center">
+        <img src="/CBC-Logo-transparent.png" className=" cursor-pointer h-full rounded-full absolute left-[10px]" />
+
+        <Link to="/" className="absolute right-[10px] text-black"> Home </Link>
+      </header>
+    );
+  }
--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -3,6 +3,7 @@ import Header from '../components/header';
 
 export default function Homepage() {
   return (
+    
     <div className="h-screen w-full bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400">
        <Header></Header>
       
