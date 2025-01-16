67c520568ce0a23b384d3d762f074819e29e09d9
Merrian Jethuni <m.j.christina321@gmail.com>
2025-01-16 23:14:53 +0530

    Created a header ✨

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,10 +1,11 @@
 import { Link, Routes, Route } from 'react-router-dom';
+import Header from '../components/header';
 
 export default function Homepage() {
   return (
     <div className="h-screen w-full bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400">
+       <Header></Header>
       
-        
     </div>
   );
 }
