4a1f527fb74d8ba9906b81b194d95bf3b3052924
Merrian Jethuni <m.j.christina321@gmail.com>
2025-01-17 20:09:03 +0530

    ✨ Added confirmation prompt for product deletion to ensure user consent 🗑️

--- src/pages/admin/adminProductsPage.jsx
+++ src/pages/admin/adminProductsPage.jsx
@@ -71,7 +71,7 @@ export default function AdminProductsPage() {
                 >
                   <FaPencilAlt />
                 </button>
-                <button
+              <button
                   className="text-red-600 hover:text-red-800"
                   title="Delete Product"
                   onClick={() => {
