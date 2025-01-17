9724ed785c72935f61115f2fa9b531a26cc8c41f
Merrian Jethuni <m.j.christina321@gmail.com>
2025-01-17 22:11:51 +0530

    💻 Added routes for the Home Page and Login Page! 🏠

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,11 +1,16 @@
 import { Link, Routes, Route } from 'react-router-dom';
 import Header from '../components/header';
+import LoginPage from './loginPage';
 
 export default function Homepage() {
   return (
     
     <div className="h-screen w-full bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400">
-       <Header></Header>
+       <Header/>
+        <Routes path="/*">
+         <Route path='/' element={<h1> Home Page </h1>} />
+         <Route path='/login' element={<LoginPage/>} />
+        </Routes>
       
     </div>
   );
