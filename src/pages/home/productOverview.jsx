873dc19d1b1050e9e333ec9f713fea0f60d77fac
Merrian Jethuni <m.j.christina321@gmail.com>
2025-01-18 19:55:46 +0530

    🚀 Refactor useEffect to remove async usage ✂️

--- src/pages/home/productOverview.jsx
+++ src/pages/home/productOverview.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useParams } from "react-router-dom"
 
 export default function ProductOverview(){
@@ -5,12 +6,16 @@ export default function ProductOverview(){
     const params = useParams();
     const productId = params.id;
 
-    useEffect(
-    async ()=> {
-        console.log(productId)
-       const data = await axios.get(import.meta.env.VITE_BACKEND_URL="/api/productId/" + productId) 
-    }
-    , [])
+    useEffect(() => {
+        console.log(productId);
+        axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/productId/${productId}`)
+            .then((response) => {
+                console.log(response.data);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }, []);
 
     return (
         <div className="w-full h-[calc (100vh-100px)] bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400" >
