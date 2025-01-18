4909c8f5afb352f36806b24b907cf6f9ffde2f72
Merrian Jethuni <m.j.christina321@gmail.com>
2025-01-18 21:34:14 +0530

    Centered loading spinner within the viewport 🔅

--- src/pages/home/productOverview.jsx
+++ src/pages/home/productOverview.jsx
@@ -24,8 +24,8 @@ export default function ProductOverview(){
         <div className="w-full h-[calc (100vh-100px)]" >
             {
                 status == "loading" && (
-                    <div className="w-full h-full flex items-center justify-center">
-                        <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900">
+                    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                        <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-2 border-gray-500 border-b-accent">
                         </div>
                     </div>
                 )
