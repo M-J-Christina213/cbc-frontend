23d0da8de39bbf1df74fca32f80f55d0cfe7ff3a
Merrian Jethuni <m.j.christina321@gmail.com>
2025-01-18 12:45:22 +0530

    Logged product ID on component load 🔍

--- src/pages/home/productOverview.jsx
+++ src/pages/home/productOverview.jsx
@@ -1,4 +1,15 @@
+import { useParams } from "react-router-dom"
+
 export default function ProductOverview(){
+
+    const params = useParams();
+    const productId = params.id;
+
+    useEffect(
+        ()=>{
+            console.log(productId)
+        }
+    , [])
     return (
         <div className="w-full h-[calc (100vh-100px)] bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400" >
             <h1> Product Overview </h1>
--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -7,7 +7,7 @@ export default function Homepage() {
   return (
     <div className="h-screen w-full">
       <Header />
-      <div className="w-full h-[calc(100vh)]">
+      <div className="w-full h-[calc(100vh)] bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400">
         <Routes>
           <Route path="/" element={<h1>Home Page</h1>} />
           <Route path="/login" element={<LoginPage />} />
