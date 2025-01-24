12f4dde429d2eae67cb02da6cba84feb904b43c8
Merrian Jethuni <m.j.christina321@gmail.com>
2025-01-24 17:38:35 +0530

    Created a productPage and a route 👍

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -2,6 +2,7 @@ import { Link, Routes, Route } from 'react-router-dom';
 import Header from '../components/header';
 import LoginPage from './loginPage';
 import ProductOverview from './home/productOverview';
+import ProductPage from './home/product';
 
 export default function Homepage() {
   return (
@@ -10,6 +11,7 @@ export default function Homepage() {
       <div className="w-full h-[calc(100vh)] bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400">
         <Routes path="/*">
           <Route path="/" element={<h1>Home Page</h1>} />
+          <Route path='/product' element={<ProductPage/>}/>
           <Route path="/login" element={<LoginPage />} />
           <Route path="/productInfo/:productID" element={<ProductOverview />} />
         </Routes>
