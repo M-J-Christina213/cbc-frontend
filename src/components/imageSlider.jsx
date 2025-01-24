78b0730e58a92afa7d380001e59b20475a74cb7a
Merrian Jethuni <m.j.christina321@gmail.com>
2025-01-24 13:27:59 +0530

    did some debugging to find why my image slider was above the header but still couldnt solve it. 🤔

--- src/components/imageSlider.jsx
+++ src/components/imageSlider.jsx
@@ -5,26 +5,26 @@ export default function ImageSlider(props) {
         <div className="w-full aspect-square flex items-center flex-col relative">
             {/* Main Image */}
             <img 
-                src={images[0] || "default-placeholder-image.jpg"} 
+                src={images[0]} 
                 className="w-full aspect-square object-cover" 
                 alt="Main Slider Image"
             />
             {/* Blur effect */}
-            <div className="w-full h-[100px] absolute bottom-0 backdrop-blur-lg"></div>
-            {/* Thumbnails */}
-            <div className="w-full h-full flex items-center justify-center">
-                {images.length ? (
-                    images.map((image, index) => (
-                        <img 
-                            key={index}
-                            src={image}
-                            className="w-12 h-12 object-cover mx-2"
-                            alt={`Thumbnail ${index + 1}`}
-                        />
-                    ))
-                ) : (
-                    <p>No images available</p>
-                )}
+            <div className="w-full h-[100px] absolute bottom-0 backdrop-blur-lg">
+                {/* Thumbnails */}
+                <div className="w-full h-full flex items-center justify-center overflow-hidden">
+                    {images.length ? (
+                        images.map((image, index) => (
+                            <img 
+                                key={index}
+                                src={image}
+                                className="w-16 h-16 cursor-pointer object-cover mx-2"
+                            />
+                        ))
+                    ) : (
+                        <p>No images available</p>
+                    )}
+                </div>
             </div>
         </div>
     );
