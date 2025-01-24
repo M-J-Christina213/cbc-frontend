93be2013c960b0b749a4a5b72d465fcb987f75f8
Merrian Jethuni <m.j.christina321@gmail.com>
2025-01-24 21:36:44 +0530

    🛍️ Implemented ProductPage with basic API call for fetching products

--- src/pages/home/product.jsx
+++ src/pages/home/product.jsx
@@ -1,4 +1,30 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import toast from "react-hot-toast"
+
 export default function ProductPage(){
+    const [products,setProducts] = useState([])
+    const [loadingStatus, setLoadingStatus] = useState([])
+    
+    useEffect (
+        ()=>{
+            if(loadingStatus==="loading"){
+                axios.get(import.meta.env.VITE_API_URL + '/api/products')
+                .then(
+                    (res)=>console.log(res.data)
+    
+                ).catch (
+                    (err)=>toast.error("Failed to fetch products")
+                  )
+                }
+                     
+                
+            }
+        
+            
+        
+
+    ,[])
     return(
         <div>
             <h1> Product page </h1>
