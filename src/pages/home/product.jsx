6424ac131fda1cd3a0bd4bfcea014eb97e6f67b4
Merrian Jethuni <m.j.christina321@gmail.com>
2025-01-25 13:37:48 +0530

    Typo error that should be backend not API 😅

--- src/pages/home/product.jsx
+++ src/pages/home/product.jsx
@@ -4,14 +4,18 @@ import toast from "react-hot-toast"
 
 export default function ProductPage(){
     const [products,setProducts] = useState([])
-    const [loadingStatus, setLoadingStatus] = useState([])
+    const [loadingStatus, setLoadingStatus] = useState('loading')
     
     useEffect (
         ()=>{
             if(loadingStatus==="loading"){
-                axios.get(import.meta.env.VITE_API_URL + '/api/products')
+                axios.get(import.meta.env.VITE_BACKEND_URL + '/api/products')
                 .then(
-                    (res)=>console.log(res.data)
+                    (res)=>{
+                    console.log(res.data)
+                    setProducts(res.data)
+                    setLoadingStatus('loaded')
+                    }
     
                 ).catch (
                     (err)=>toast.error("Failed to fetch products")
@@ -25,9 +29,19 @@ export default function ProductPage(){
         
 
     ,[])
-    return(
-        <div>
-            <h1> Product page </h1>
+    return (
+        <div className="w-full h-full bg-blue-300 overflow-y-scroll">
+            {products.map(
+                (product)=>
+                    <div key={product.id} className="flex flex-col items-center">
+                        <img src={product.image} alt={product.name} className="h-40 w-40 object-cover"/>
+                        <h1>{product.name}</h1>
+                        <p>{product.description}</p>
+                        <p>{product.price}</p> 
+                    </div>    
+            )
+            }
         </div>
-    )
+    );
+    
 }
