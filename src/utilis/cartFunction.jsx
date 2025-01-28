72998edb51f604d0a8062d8b7c34806d14cf8893
Merrian Jethuni <m.j.christina321@gmail.com>
2025-01-28 19:16:28 +0530

    💾 Added loadCart and addToCart functions for cart handling. 😎

--- src/utilis/cartFunction.jsx
+++ src/utilis/cartFunction.jsx
@@ -1,3 +1,24 @@
 function loadCart(){
     const cart = localStorage.getItem("cart");
+    if(cart!=null){
+        return JSON.parse(cart)
+    }else {
+        return []
+    }
+}
+
+export function addToCart(productId, qty)
+{
+    const cart = loadCart()
+
+    const index = cart.findIndex(
+        (item)=>{item.productId==productId
+
+        }
+    )
+    if(index==-1){
+        cart.push(
+            {productId, qty}
+        )
+    }
 }
