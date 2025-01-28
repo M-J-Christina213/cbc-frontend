439a33f6fdb70f6e6073000ad38ea331be36d3b8
Merrian Jethuni <m.j.christina321@gmail.com>
2025-01-28 19:46:04 +0530

    added useState and effect for the cart to check the cart is loaded 😉

--- src/pages/home/cart.jsx
+++ src/pages/home/cart.jsx
@@ -4,12 +4,20 @@ export default function Cart(){
     const [cart, setCart] = useState([])
     useEffect(
         ()=>{
-            setCart(loadCart)
+            setCart(loadCart())
         }
     )
     return (
         <div className="w-full h-full overflow-y-scroll flex flex-wrap justify-center" >
-            <h1> Cart </h1>
+            {
+                cart.map(
+                    (item)=>{
+                        return (
+                            <span> {item.productId} X {item.qty} </span>
+                        )
+                    }
+                )
+            }
         </div>
     )
 }
