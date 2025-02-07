1cd5081dcbb415c0f0fd8571e294943a928f1510
Merrian Jethuni <m.j.christina321@gmail.com>
2025-02-07 22:31:39 +0530

    RxHamburgerMenu is defined 😅

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,13 @@
+import { RxHamburgerMenu } from "react-icons/rx";
 import {Link } from "react-router-dom"
 
 export default function Header() {
     return (
       <header className="bg-white w-full h-[100px] relative flex justify-center items-center">
         <img src="/CBC-Logo-transparent.png" className=" cursor-pointer h-full rounded-full absolute left-[10px]" />
-        
+        <RxHamburgerMenu className="text-3x1 absolute right-[10px]"/>
         <div className="h-full hidden lg:flex items-center w-[500px] justify-between">
+    
         <Link to="/" className="text-primary font-bold text-x1 hover:border-b-2  border-b-secondary"> Home </Link>
         <Link to="/products" className="text-primary  font-bold text-x1 hover:border-b-2 border-b-secondary"> Products </Link>
         <Link to="/aboutUs" className="text-primary font-bold text-x1 hover:border-b-2 border-b-secondary"> About Us </Link>
