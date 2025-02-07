3b485de247cdf491eac9296319ed58543fa68e5c
Merrian Jethuni <m.j.christina321@gmail.com>
2025-02-07 22:11:46 +0530

    to display the header as flex and no visible in mobile 🎞️

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
       <header className="bg-white w-full h-[100px] relative flex justify-center items-center">
         <img src="/CBC-Logo-transparent.png" className=" cursor-pointer h-full rounded-full absolute left-[10px]" />
         
-        <div className="h-full flex items-center w-[500px] justify-between">
+        <div className="h-full hidden lg:flex items-center w-[500px] justify-between">
         <Link to="/" className="text-primary font-bold text-x1 hover:border-b-2  border-b-secondary"> Home </Link>
         <Link to="/products" className="text-primary  font-bold text-x1 hover:border-b-2 border-b-secondary"> Products </Link>
         <Link to="/aboutUs" className="text-primary font-bold text-x1 hover:border-b-2 border-b-secondary"> About Us </Link>
