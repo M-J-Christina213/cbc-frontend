eed0dbea3758bd66567a2978b9cab515fa3df0a7
Merrian Jethuni <m.j.christina321@gmail.com>
2025-02-12 21:35:09 +0530

    Enhance ShippingPage to display cart items and total calculations 🎨

--- src/pages/home/shipping.jsx
+++ src/pages/home/shipping.jsx
@@ -1,7 +1,50 @@
+import toast from "react-hot-toast";
+import { useLocation, useNavigate } from "react-router-dom"
+import CartCard from "../../components/cartCard";
+
 export default function ShippingPage(){
+    const location = useLocation();
+    const navigate = useNavigate();
+    const cart = location.state.items
+
+    if (cart==null){
+        toast.error("No items received")
+        navigate("/cart")
+    }
     return(
     <div className="w-full h-full bg-red-900">
-        <h1> Shipping Page </h1>
+        <div className="w-full h-full overflow-y-scroll flex flex-col items-end">
+                    <table className="w-full bg-accent">
+                        <thead>
+                            <tr>
+                                <th> Image </th>
+                                <th> Product Name </th>
+                                <th> Product ID</th>
+                                <th> Qty </th>
+                                <th> Price </th>
+                                <th> Total </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {cart.map((item) => (
+                                <CartCard key={item.productId} productID={item.productId} qty={item.qty} />
+                            ))}
+                        </tbody>
+                    </table>
+                    <h1 className="text-3xl font-bold text-primary">
+                        Total: Rs. {Number(labeledTotal).toFixed(2)}
+                    </h1>
+                    <h1 className="text-3xl font-bold text-primary">
+                            Discount: Rs. {Number(labeledTotal - total).toFixed(2)}
+                    </h1>
+                    <h1 className="text-3xl font-bold text-primary">
+                            GrandTotal: Rs. {Number(total).toFixed(2)}
+                    </h1>
+        
+                    <button onClick={onOrderCheckOutClick} className="bg-primary text-white p-2 rounded-lg w-[300px]"> Checkout </button>
+                </div>
     </div>
+
+    
     )
 }
