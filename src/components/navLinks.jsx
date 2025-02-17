14071efb967d23305e29536111836cd34118484a
M-J-Christina213 <m.j.christina213@gmail.com>
2025-02-17 17:37:41 +0530

    add NavLinks component with navigation items 🔗

---
+++ src/components/navLinks.jsx
@@ -0,0 +1,19 @@
+import react from 'react';
+
+const NavLinks =() => {
+    const Links = [
+         {name: 'Home'},
+        {name: 'Shop'},
+        {name: 'Makeup'},
+        {name: 'Skincare'},
+        {name: 'Haircare'},
+        {name: 'Fragrances'},
+        {name: 'Bath & Body'},
+        {name: 'New Arrivals'},
+        {name: 'Special Offers'},
+        {name: 'Gifts'},
+        {name: 'About Us'},
+        {name: 'Contact Us'},
+
+    ]
+}
