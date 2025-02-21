3f77ccc31332fd9551637c9cfb6d092b10afc489
M-J-Christina213 <m.j.christina213@gmail.com>
2025-02-21 18:50:37 +0530

    😮‍💨Added an example format and layout 😪

--- src/components/productCategories.jsx
+++ src/components/productCategories.jsx
@@ -1,19 +1,24 @@
 export default function ProductCategories(){
 
     return (
-        <div>
+        <div className="mt-14 mb-12"> 
 
-        <div>
+        <div className="container">
             {/*Header Section */}
             <div className="text-center mb-10 max-w-[600px]">
                 <p className="text-sm text-primary" > Top Selling Product Categories </p>
-                <h1> Products </h1>
-                <p>
+                <h1 className="text-3xl font-bold"> Products </h1>
+                <p className="text-xs text-gray-500"> 
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                    Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                </p>
+                    </p>
+                    
             </div>
             {/*Body Section */}
+            <div>
+                <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 place-items-center gap-5">
+                    {/*Card Section */}
+                </div>
+            </div>
 
         </div>
     </div>
