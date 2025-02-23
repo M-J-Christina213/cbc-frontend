34ffab11fb55f36ed3639b86b9a74693dab73d35
M-J-Christina213 <m.j.christina213@gmail.com>
2025-02-23 17:35:56 +0530

    😭issue with localhost 😥

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import AdminHomepage from './pages/admin/adminHomePage'
 import { Toaster } from 'react-hot-toast'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import AOS from "aos";
+import 'aos/dist/aos.css';
 
 
 function App() {
